2383b616ad15c39990f3eaddb1078da1084e9007
mhairi19 <166132125+mhairi19@users.noreply.github.com>
2026-04-08 00:43:58 +0800

    fix: update routes for dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
- 
+
 use App\Http\Controllers\FuelController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
- 
+
 Route::get('/', function () {
-	return view('welcome');
+    return redirect('/login');
 });
- 
+
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', [FuelController::class, 'index'])
          ->name('dashboard');
@@ -14,6 +15,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
          ->name('fuel.store');
     Route::delete('/fuel-entries/{fuelEntry}', [FuelController::class, 'destroy'])
          ->name('fuel.destroy');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
- 
+
 require __DIR__.'/auth.php';
